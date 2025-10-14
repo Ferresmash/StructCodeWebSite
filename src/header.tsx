a7b97b9fc362381dd9d45be389d4ae574e52c1d9
Ferresmash <120678062+Ferresmash@users.noreply.github.com>
2025-10-14 16:58:20 +0200

    a lil error

---
+++ src/header.tsx
@@ -0,0 +1,11 @@
+//small header with site title and navigation links
+export function Header() {
+    return (
+        <header class="w-full p-4 bg-black/50 backdrop-blur-3xl flex justify-between items-center">
+            <h1 class="text-2xl font-bold">Silas AB</h1>
+            <nav>   
+
+            </nav>
+        </header>
+    )
+}
