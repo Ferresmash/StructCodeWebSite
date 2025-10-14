2ced3e9fefebf1e0bfe0cd02d42db2e542e4ba68
Ferresmash <120678062+Ferresmash@users.noreply.github.com>
2025-10-14 19:50:06 +0200

    added deploy

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,8 +8,8 @@
      return (
         <div className={"flex flex-col items-center justify-center gap-4 size-full bg-black/85 backdrop-blur-xl xl:backdrop-blur-2xl 2xl:backdrop-blur-3xl"}>
         <div className={"flex flex-col items-center bg-black/30 p-5 rounded-xl"}>
-          <p className="font-bold text-5xl pb-2">Silas AB</p>
-          <p className="font-bold text-3xl text-center max-w-[600px]">AI-genererad kod som kommer hålla max en vecka, helt gratis!</p>
+          <p className="font-bold text-5xl pb-2">Låt oss hjälpa dig!</p>
+          <p className="font-bold text-3xl text-center max-w-[600px]">Vi tar din idé till en verklig produkt!</p>
 
         </div>
       </div>
