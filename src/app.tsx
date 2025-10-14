a779617097ed2b5e807c5e10677dd977571e7272
GTSA - Infinity <williamlindahl03@gmail.com>
2025-10-14 16:40:39 +0200

    added background image with blur effect

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,11 @@ export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-      
-    </>
+    <div className="w-screen h-screen bg-[url(green-light.jpg)] bg-center bg-cover">
+      <div className={"flex flex-col items-center justify-center gap-4 size-full bg-black/80 backdrop-blur-3xl"}>
+        <p className="font-bold text-5xl">Silas AB</p>
+        <p className="font-bold text-3xl text-center max-w-[600px]">AI-genererad kod som kommer hålla max en vecka, helt gratis!</p>
+      </div>
+    </div>
   )
 }
