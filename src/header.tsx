1215afec1e896c4fc6bcb915d818b4ce7f82fd8e
GTSA - Infinity <williamlindahl03@gmail.com>
2025-10-14 17:41:07 +0200

    styling the header

--- src/header.tsx
+++ src/header.tsx
@@ -1,10 +1,12 @@
 //small header with site title and navigation links
 export function Header() {
     return (
-        <header class="w-full p-4 bg-black/50 backdrop-blur-3xl flex justify-between items-center">
-            <h1 class="text-2xl font-bold">Silas AB</h1>
+        <header className="max-w-[900px] w-full px-4 py-2 flex justify-between items-center fixed top-0">
+            <p className="text-3xl font-bold">Silas AB</p>
             <nav>   
-
+                <a href="#home" class="text-white hover:underline mx-2 ">Home</a>
+                <a href="#about" className="text-white hover:underline mx-2">About</a>
+                <a href="#contact" className="text-white hover:underline mx-2">Contact</a>
             </nav>
         </header>
     )
