7c3d9c257fdcec291693ca4847bbce3de1d7c25f
GTSA - Infinity <williamlindahl03@gmail.com>
2025-10-14 19:03:31 +0200

    Made header darker on scroll

--- src/contact.tsx
+++ src/contact.tsx
@@ -8,8 +8,10 @@
      return (
         <div className={"flex flex-col items-center justify-center gap-4 size-full bg-neutral-950"}>
         <div className={"flex flex-col items-center bg-black/30 p-5 rounded-xl"}>
-          <p className="font-bold text-5xl pb-2">Kontakta oss</p>
-          <p className="font-bold text-3xl text-center max-w-[600px]">ndwkladjkaw@gmail.com</p>
+            <label className="font-bold text-5xl pb-2">Kontakt</label>
+            <input type="text" placeholder="Namn" className="p-2 rounded-md mb-2 text-white"/>
+            <label className="font-bold text-3xl text-center max-w-[600px]">Fyll i dina kontaktuppgifter så hör vi av oss!</label>
+            <input type="text" placeholder="Namn" className="p-2 rounded-md mb-2 text-white"/>
 
         </div>
       </div>
