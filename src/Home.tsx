a827ba012c2c7a337f76b4b1a11e5cfea672fb41
Ferresmash <120678062+Ferresmash@users.noreply.github.com>
2025-10-14 18:07:05 +0200

    finally working

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-   import { Header } from './Header.tsx'
+   import { Header } from './header.tsx'
 
    
    export function Home() {
@@ -8,7 +8,7 @@
      return (
         <div className="w-screen h-screen bg-[url(green-light.jpg)] bg-center bg-cover">
         <div className={"flex flex-col items-center justify-center gap-4 size-full bg-black/80 backdrop-blur-3xl"}>
-            <p className="font-bold text-5xl text-black">Silas AB</p>
+            <p className="font-bold text-5xl text-white">Silas AB</p>
             <p className="font-bold text-3xl text-center max-w-[600px]">AI-genererad kod som kommer hålla max en vecka, helt gratis!</p>
         </div>
         </div>
--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,16 @@
 import './app.css'
-import { Header } from './Header.tsx'
+import { Header } from './header.tsx'
 
 export function App() {
   return (
     <div className="w-screen h-screen bg-[url(green-light.jpg)] bg-center bg-cover">
       <Header/>
       <div className={"flex flex-col items-center justify-center gap-4 size-full bg-black/85 backdrop-blur-xl xl:backdrop-blur-2xl 2xl:backdrop-blur-3xl"}>
-        <p className="font-bold text-5xl">Silas AB</p>
-        <p className="font-bold text-3xl text-center max-w-[600px]">AI-genererad kod som kommer hålla max en vecka, helt gratis!</p>
+        <div className={"flex flex-col items-center bg-black/30 p-5 rounded-xl"}>
+          <p className="font-bold text-5xl">Silas AB</p>
+          <p className="font-bold text-3xl text-center max-w-[600px]">AI-genererad kod som kommer hålla max en vecka, helt gratis!</p>
+
+        </div>
       </div>
     </div>
 
