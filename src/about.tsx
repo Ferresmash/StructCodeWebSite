46f51b1eb2b1f398632c9ae121823b9cb2e5c88f
Ferresmash <120678062+Ferresmash@users.noreply.github.com>
2025-10-14 19:04:07 +0200

    added image and icons

--- src/about.tsx
+++ src/about.tsx
@@ -6,12 +6,36 @@
     
    
      return (
-        <div className={"flex flex-col items-center justify-center gap-4 size-full bg-neutral-900"}>
-        <div className={"flex flex-col items-center bg-black/30 p-5 rounded-xl"}>
+        <div className={"flex flex-col items-center justify-center gap-4 bg-neutral-900 "}>
+        <div className={"flex flex-col items-center  p-5 rounded-xl mt-10"}>
           <p className="font-bold text-5xl pb-2">Om oss</p>
           <p className="font-bold text-3xl text-center max-w-[600px]">Vi är experter på allting</p>
 
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-10">
+        <div className={"flex flex-col items-center hover:bg-black/30  p-5 rounded-xl"}>
+          <img  src="/ferreicon.jpg" className="w-32 h-32 mb-4 rounded-full"/>
+          <p className="font-bold text-2xl pb-2">Ferdinand Öhrn</p>
+          <p className="text-center max-w-[200px] text-neutral-400">"Allt kan göras om man vågar!"</p>
+        </div>
+        <div className={"flex flex-col items-center  p-5 rounded-xl"}>
+          <img  src="/William.jpg" className="w-32 h-32 mb-4 rounded-full"/>
+          <p className="font-bold text-2xl pb-2">William Lindahl</p>
+          <p className="text-center max-w-[200px] text-neutral-400">"Att lära många ting lär oss inte att förstå"</p>
+        </div>
+        <div className={"flex flex-col items-center  p-5 rounded-xl"}>
+          <img  src="/pontusicon.png" className="w-32 h-32 mb-4 rounded-full"/>
+          <p className="font-bold text-2xl pb-2">Pontus Lindholm</p>
+          <p className="text-center max-w-[200px] text-neutral-400">"Livet är inte ett problem som skall lösas utan en verklighet som skall upplevas"</p>
+        </div>
+        <div className={"flex flex-col items-center p-5 rounded-xl"}>
+          <img  src="/axelicon.png" className="w-32 h-32 mb-4 rounded-full"/>
+          <p className="font-bold text-2xl pb-2">Axel Edner</p>
+          <p className="text-center max-w-[200px] text-neutral-400">"Livet är kort. Låt oss säga sanningen"</p>
+        </div>
+        </div>
+
+        
       </div>
    
      )
--- src/header.tsx
+++ src/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
         <header className="w-full px-4 py-2 flex justify-center items-center fixed top-0 z-10">
             <div className={"max-w-[800px] w-full flex flex-row justify-between"}>
                 <p className="text-3xl font-bold">Silas AB</p>
-                <nav>   
+                <nav className={"p-2"}>   
                     <a href="#home" class="text-white hover:underline mx-2 ">Home</a>
                     <a href="#about" className="text-white hover:underline mx-2">About</a>
                     <a href="#contact" className="text-white hover:underline mx-2">Contact</a>
