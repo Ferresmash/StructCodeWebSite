266e2c4891f2e14202c078ee46ad74b8625102cb
GTSA - Infinity <williamlindahl03@gmail.com>
2025-10-21 21:57:23 +0200

    added logo

--- src/header.tsx
+++ src/header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "preact/hooks";
+import logo from './assets/logo.png';
 
 //small header with site title and navigation links
 export function Header() {
@@ -21,9 +22,10 @@ export function Header() {
     return (
         <header className={`w-full px-4 py-2 flex max-w-screen justify-center fixed top-0 z-10 transition-colors duration-300
         ${scrolled ? "bg-black/50 backdrop-blur-md shadow-md" : "bg-transparent" }`}>
-            <div className={"max-w-[1000px] w-full flex flex-row justify-between items-center"}>
+            <div className={"max-w-[1000px] w-full flex flex-row gap-2 justify-start items-center"}>
+                <img src={logo} alt="Logo" className="w-8 h-8"/>
                 <p className="text-3xl font-bold">STRUKTURɅ</p>
-                <nav className={"p-2"}>   
+                <nav className={"p-2 ml-auto"}>   
                     <button onClick={() => scrollToSection("home")} class="text-white hover:underline mx-2 cursor-pointer">Hem</button>
                     <button onClick={() => scrollToSection("about")} className="text-white hover:underline mx-2 cursor-pointer">Om oss</button>
                     <button onClick={() => scrollToSection("contact")} className="text-white hover:underline mx-2 cursor-pointer">Kontakta</button>
